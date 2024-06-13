11afc6a2ea8f17a6200094bd265ceea0fadb0a1d
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-06-13 22:13:50 +0600

    finish Video но осталас инфо видео

--- src/components/vacancyList/vacancyList.tsx
+++ src/components/vacancyList/vacancyList.tsx
@@ -2,65 +2,82 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import "./vacancyList.css";
 import { useNavigate } from "react-router-dom";
-
 interface Vacancy {
-  id: number;
-  logo?: string;
-  organization: string;
-  office: string;
-  salary: string | number;
-  jobType: string;
+    id: number;
+    logo: string;
+    organization: string;
+    office: string;
+    salary: string | number;
+    jobType: string;
 }
 
 const VacancyList: React.FC = () => {
-  const [data, setData] = useState<Vacancy[]>([]);
-  const navigate = useNavigate();
+    const [data, setData] = useState<Vacancy[]>([]);
+    const navigate = useNavigate();
 
-  const handleVacancyClick = (id: number) => {
-    navigate(`/JobPage/${id}`);
-  };
+    const handleVacancyClick = (id: number) => {
+        navigate(`/JobPage/${id}`);
+    };
 
-  useEffect(() => {
-    axios("https://01de09931cc9286e.mokky.dev/allvakansies")
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((err) => {
-        console.error("Error fetching data:", err);
-      });
-  }, []);
+    useEffect(() => {
+        axios("https://01de09931cc9286e.mokky.dev/allvakansies")
+            .then((res) => {
+                setData(res.data);
+            })
+            .catch((err) => {
+                console.error("Error fetching data:", err);
+            });
+    }, []);
 
-  return (
-    <div className="VacancyList">
-      {data.map((vacancy) => (
-        <div
-          key={vacancy.id}
-          className="VacancyList__item"
-          onClick={() => handleVacancyClick(vacancy.id)}
-        >
-          <div className="vacancy_logo">
-            {/* <img src={logoImg} /> */}
-          </div>
-          <div className="VacancyList__details">
-            <b className="VacancyList__title">Компания</b>
-            <b className="VacancyList__name">{vacancy.organization}</b>
-          </div>
-          <div className="VacancyList__details">
-            <b className="VacancyList__title">Должность</b>
-            <b className="VacancyList__office">{vacancy.office}</b>
-          </div>
-          <div className="VacancyList__details">
-            <b className="VacancyList__title">Оклад</b>
-            <b className="VacancyList__salary">{vacancy.salary}</b>
-          </div>
-          <div className="VacancyList__details">
-            <b className="VacancyList__title">Тип</b>
-            <b className="VacancyList__type">{vacancy.jobType}</b>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+    return (
+        <>
+            <div className="container">
+                <div className="VacancyList">
+                    {data.map((vacancy) => (
+                        <div
+                            key={vacancy.id}
+                            className="VacancyList__item"
+                            onClick={() => handleVacancyClick(vacancy.id)}
+                        >
+                            <div className="vacancy_logo">
+                                <img src={vacancy.logo} />
+                            </div>
+                            <div className="logo-detalils">
+                                <div className="VacancyList__details">
+                                    <b className="VacancyList__title">
+                                        Компания
+                                    </b>
+                                    <b className="VacancyList__name">
+                                        {vacancy.organization}
+                                    </b>
+                                </div>
+                                <div className="VacancyList__details">
+                                    <b className="VacancyList__title">
+                                        Должность
+                                    </b>
+                                    <b className="VacancyList__office">
+                                        {vacancy.office}
+                                    </b>
+                                </div>
+                                <div className="VacancyList__details">
+                                    <b className="VacancyList__title">Оклад</b>
+                                    <b className="VacancyList__salary">
+                                        {vacancy.salary}
+                                    </b>
+                                </div>
+                                <div className="VacancyList__details">
+                                    <b className="VacancyList__title">Тип</b>
+                                    <b className="VacancyList__type">
+                                        {vacancy.jobType}
+                                    </b>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default VacancyList;
--- src/pages/JobOpenings/JobOpenings.tsx
+++ src/pages/JobOpenings/JobOpenings.tsx
@@ -3,28 +3,29 @@ import Header from "../../components/header/header";
 import VacancyList from "../../components/vacancyList/vacancyList";
 import { useNavigate } from "react-router-dom";
 import Footer from "../../components/footer/footer";
-import './JobOpenings.css'
+import "./JobOpenings.css";
 
 function JobOpenings() {
-  const nav = useNavigate();
+    const nav = useNavigate();
 
-  const AddVacancy = () => {
-    nav("/AddVacancy");
-  };
+    const AddVacancy = () => {
+        nav("/AddVacancy");
+    };
 
-  return (
-    <div>
-      <Header />
-      <div className="JobOpenings_content">
-        <button className="vacancy-btn" onClick={AddVacancy}>
-          Добавить вакансию
-        </button>
-        <VacancyList />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <div className="JobOpenings_content">
+                    <button className="vacancy-btn" onClick={AddVacancy}>
+                        Добавить вакансию
+                    </button>
+                </div>
+            </div>
+            <VacancyList />
+            <Footer />
+        </>
+    );
 }
 
 export default JobOpenings;
-
