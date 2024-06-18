d02aaf794c4b425ff95e3f2c1f889fe73a857eed
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-06-18 15:43:58 +0600

    finish vacancy info

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,118 +1,15 @@
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
+import Hero from "../../components/hero/hero";
 import "./Events.css";
 function Events() {
-  return (
-    <>
-      <Header />
-      <div id="add-viv">
-        <div className="container">
-          <div className="btn-vk">
-            <button>Добавить вакансию</button>
-          </div>
-
-          <div className="add-viv">
-            <div className="mainBlock">
-              <h4>
-                10 июня 2024 <i>18:30</i>
-              </h4>
-              <div className="text">
-                <h1>Что такое SQL и почему он нужен везде? </h1>
-                <div className="textInfo">
-                  <div className="">
-                    <p>организатор</p>
-                    <h1>Mega Lab </h1>
-                  </div>
-                  <div className="">
-                    <p>Локация</p>
-                    <h1>Суюнбаева 123(головной офис ) </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="add-viv">
-            <div className="mainBlock">
-              <h4>
-                10 июня 2024 <i>18:30</i>
-              </h4>
-              <div className="text">
-                <h1>Что такое SQL и почему он нужен везде? </h1>
-                <div className="textInfo">
-                  <div className="">
-                    <p>организатор</p>
-                    <h1>Mega Lab </h1>
-                  </div>
-                  <div className="">
-                    <p>Локация</p>
-                    <h1>Суюнбаева 123(головной офис ) </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="add-viv">
-            <div className="mainBlock">
-              <h4>
-                10 июня 2024 <i>18:30</i>
-              </h4>
-              <div className="text">
-                <h1>Что такое SQL и почему он нужен везде? </h1>
-                <div className="textInfo">
-                  <div className="">
-                    <p>организатор</p>
-                    <h1>Mega Lab </h1>
-                  </div>
-                  <div className="">
-                    <p>Локация</p>
-                    <h1>Суюнбаева 123(головной офис ) </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> <div className="add-viv">
-            <div className="mainBlock">
-              <h4>
-                10 июня 2024 <i>18:30</i>
-              </h4>
-              <div className="text">
-                <h1>Что такое SQL и почему он нужен везде? </h1>
-                <div className="textInfo">
-                  <div className="">
-                    <p>организатор</p>
-                    <h1>Mega Lab </h1>
-                  </div>
-                  <div className="">
-                    <p>Локация</p>
-                    <h1>Суюнбаева 123(головной офис ) </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> <div className="add-viv">
-            <div className="mainBlock">
-              <h4>
-                10 июня 2024 <i>18:30</i>
-              </h4>
-              <div className="text">
-                <h1>Что такое SQL и почему он нужен везде? </h1>
-                <div className="textInfo">
-                  <div className="">
-                    <p>организатор</p>
-                    <h1>Mega Lab </h1>
-                  </div>
-                  <div className="">
-                    <p>Локация</p>
-                    <h1>Суюнбаева 123(головной офис ) </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Footer />
+        </>
+    );
 }
 
 export default Events;
