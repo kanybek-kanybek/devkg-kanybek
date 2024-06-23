5022413d3d7dc7f085c3110960a4e8cf2c82875d
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-06-23 21:32:51 +0600

    vacancy finish

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,7 +1,6 @@
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
 import Hero from "../../components/hero/hero";
-import "./Events.css";
 function Events() {
     return (
         <>
